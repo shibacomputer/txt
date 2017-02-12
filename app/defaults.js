59791c174871391e3d5c30e57e1aa86f6e338a77
Cade <cade@shiba.computer>
2017-02-12 13:16:52 +1100

    🖼frame setup

--- app/defaults.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  windowSize: {
-    home:  {x: 1000, y: 700, minX: 468, minY: 240},
-  }
-}
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const { app, shell, Menu, ipcMain } = require('electron')
 const window = require('electron-window')
 const budo = require('budo')
-const path = require('path')
+
+// Get setup items.
+const windowConfig = require('./config/defaults')
+const menuConfig = require('./config/menu')
 
 const mainWindowSetup = {
-  titleBarStyle: 'hidden-inset'
+  titleBarStyle:  windowConfig.win.app.titleBar,
+  width:          windowConfig.win.app.x,
+  height:         windowConfig.win.app.y,
+  minWidth:       windowConfig.win.app.minX,
+  minHeight:      windowConfig.win.app.minY,
+  frame:          windowConfig.win.app.frame
 }
 
 let mainWindow
@@ -24,6 +32,7 @@ app.on('ready', () => {
       server.close()
     })
   })
+
   .on('update', function(file, contents) {
     mainWindow.reload()
   })
