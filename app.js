f7d2539b74be3d03ba366e29b9811d8467a8f1b5
Cade <cade@shiba.computer>
2017-02-13 11:51:24 +1100

    Base styles now working. 👩‍🎨

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const choo = require('choo')
+const mount = require ('choo/mount')
 const html = require('bel')
 const yo = require('yo-yo')
 const css = require('sheetify')
+
 const persist = require('choo-persist')
 
 css('tachyons')
@@ -12,6 +14,10 @@ const app = choo()
 
 persist((persist) => {
   app.use(persist)
-  const tree = app.start()
-  document.body.appendChild(tree)
+
+  app.router([
+    '/', require('./windows/main')
+  ])
+  
+  mount('body', app.start())
 })
--- windows/main.js
+++ windows/main.js
@@ -5,17 +5,20 @@ const css = require('sheetify')
 
 const base = css`
   :host {
-    height: 100vh;
-    background-color: var(--k)
-    display: flex;
-    flex-direction: column;
-    -webkit-app-region: drag;
+    body {
+      height: 100vh;
+      background-color: var(--k);
+      display: flex;
+      flex-direction: column;
+      -webkit-app-region: drag;
+    }
   }
 `
 module.exports = function mainWindow(state, prev, send) {
   document.title = 'Text'
   return html`
-    <body>
+    <body class="b-myc">
+
     </body>
   `
 }
