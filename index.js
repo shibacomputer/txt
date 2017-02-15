cf4e6d5e3cc5aa7190df5f234a2e641487168640
Cade <cade@shiba.computer>
2017-02-15 12:35:53 +1100

    Added beginning stages of multi-window support

--- index.js
+++ index.js
@@ -7,17 +7,22 @@
 
 */
 
+// App setup
 const { app, shell, Menu, ipcMain } = require('electron')
-const db = require('toiletdb')
 const window = require('electron-window')
+
+// Browserify service for development
 const budo = require('budo')
 
 // Get setup items.
 const windowConfig = require('./config/defaults')
 const menuConfig = require('./config/menu')
 
+// Settings
+const settings = require('electron-settings')
+
 // Setup windows
-const mainWindowSetup = {
+const mainWindowConfig = {
   titleBarStyle:  windowConfig.win.app.titleBar,
   width:          windowConfig.win.app.x,
   height:         windowConfig.win.app.y,
@@ -25,6 +30,36 @@ const mainWindowSetup = {
   minHeight:      windowConfig.win.app.minY,
   frame:          windowConfig.win.app.frame
 }
+
+const setupWindowConfig = {
+  titleBarStyle:  windowConfig.win.setup.titleBar,
+  width:          windowConfig.win.setup.x,
+  height:         windowConfig.win.setup.y,
+  minWidth:       windowConfig.win.setup.minX,
+  minHeight:      windowConfig.win.setup.minY,
+  frame:          windowConfig.win.setup.frame,
+  center:         windowConfig.win.setup.centered,
+  resizable:      windowConfig.win.setup.canResize,
+  fullscreenable: windowConfig.win.setup.fullscreen,
+  minimizable:    windowConfig.win.setup.minimizable,
+  maximizable:    windowConfig.win.setup.maximizable
+
+}
+
+const lockWindowConfig = {
+  titleBarStyle:  windowConfig.win.lock.titleBar,
+  width:          windowConfig.win.lock.x,
+  height:         windowConfig.win.lock.y,
+  minWidth:       windowConfig.win.lock.minX,
+  minHeight:      windowConfig.win.lock.minY,
+  frame:          windowConfig.win.lock.frame,
+  center:         windowConfig.win.lock.centered,
+  movable:        windowConfig.win.lock.moveable,
+  fullscreenable: windowConfig.win.lock.fullscreen,
+  minimizable:    windowConfig.win.lock.minimizable,
+  maximizable:    windowConfig.win.lock.maximizable
+}
+
 const opts = {
   debug: process.env.NODE_ENVIRONMENT !== 'production',
   verbose: true,
@@ -33,29 +68,33 @@ const opts = {
   browserify: {
     transform: [ 'sheetify/transform' ],
     insertGlobalVars: {
-      process: function() { return;
+      process: function() { return; }
     }
-  }
-
   },
   browserifyArgs: ['--im', '--no-builtins', '--ig', '--no-bf']
-};
+}
 
-let mainWindow
+let mainWin
+let lockWin
+let setupWin
 
 app.on('ready', () => {
-  mainWindow = window.createWindow(mainWindowSetup)
+  // Get our windows in order, since any of the three can be called at any time.
+  mainWin = window.createWindow(mainWindowConfig)
+  setupWin = window.createWindow(setupWindowConfig)
+  lockWin = window.createWindow(lockWindowConfig)
   var server = budo('app.js', opts)
 
+  // @TODO: Set prod vs dev settings - including dev tools.
   .on('connect', function (ev) {
-    mainWindow.showUrl(ev.uri)
-    mainWindow.webContents.openDevTools({ mode: 'detach' })
-    mainWindow.once('close', function () {
+    mainWin.showUrl(ev.uri)
+    mainWin.webContents.openDevTools({ mode: 'detach' })
+    mainWin.once('close', function () {
       server.close()
     })
   })
 
   .on('update', function(file, contents) {
-    mainWindow.reload()
+    mainWin.reload()
   })
 })
