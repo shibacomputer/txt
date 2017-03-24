5a8271d82e8485043a1a6378771a8d9f4dd825b4
Cade <cade@shiba.computer>
2017-03-24 23:32:30 +1100

    Breaking ground with the choo-5 refactor

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const choo = require('choo')
-const mount = require ('choo/mount')
-const log = require('choo-log')
-
-const css = require('sheetify')
+const choo    = require('choo'),
+      expose  = require('choo-expose'),
+      log     = require('choo-log'),
+      persist = require('choo-persist'),
+      css     = require('sheetify')
 
 // Setup global CSS
-css('tachyons')
+/*
 css('./css/color.css')
 css('./css/frame.css')
 css('./css/common.css')
 css('./css/editor.css')
+*/
+
+css('./css/defs.css')
+css('./css/common.css')
 
 const app = choo()
+app.use(persist())
 app.use(log())
+app.use(expose())
 
-app.model(require('./models/global')())
-app.model(require('./models/filesystem')())
-app.model(require('./models/note')())
+app.route('/', require('./windows/setup'))
+app.route('/setup', require('./windows/setup'))
 
-app.router({ default: '/' }, [
-  ['/', require('./windows/main')],
-  ['/setup', require('./windows/setup')]
-])
-
-mount('body', app.start())
+app.mount('body')
