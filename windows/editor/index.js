62cc6cb729ec7a9ce8958fea308c3f23f3da8368
Cade <cade@shiba.computer>
2017-08-21 22:51:45 +0200

    Working on the editor

--- windows/editor/index.js
+++ windows/editor/index.js
@@ -5,51 +5,56 @@ const strings = require('./strings')
 const icons = require('../../utils/icons')
 const button = require('../../components/button')
 const toolbar = require('../../components/toolbar')
-const tree = require('../../components/tree')
-const sidebarView = require('../../components/sidebar-view')
-const editorView = require('../../components/editor-view')
+const view = require('../../components/view')
 
 module.exports = mainWindow
 
 function mainWindow(state, emit) {
   document.title = 'Keyp'
 
+  var editorView = buildEditorView()
+
   // Set up our components
-  const browserToolbar = toolbar( null,
-    [
-      button({
-        name: 'new',
-        classes: 'c',
-        icon: 'new',
-        click: function() {
-          console.log(state.sys.path.selected)
-          emit('fs:make', state.sys.path.selected)
-        }
-      })
-    ],
-    strings.en.title,
-    emit )
-  const browserFooter = toolbar ()
-  const sidebar = {
-    toolbar: browserToolbar,
-    footer: browserFooter,
-    view: tree(state, emit)
-  }
-
-  const editorToolbar = toolbar( null, null, null, emit)
-
-
-  const editor = {
-    toolbar: editorToolbar,
-    footer: toolbar ()
-  }
-
-
   return html`
     <body class="b-myc ${style}">
       ${ icons() }
-      ${ sidebarView(state, emit, 'sidebar', sidebar) }
-      ${ editorView(state, emit, 'editor', editor) }
+      ${ editorView(state, emit, 'editor', editorView) }
     </body>
   `
+
+  function buildEditorView() {
+    var elements = {}
+
+    // Set up our components
+    const browserToolbar = toolbar( null,
+      [
+        button({
+          name: 'new',
+          classes: 'c',
+          icon: 'new',
+          click: function() {
+            console.log(state.sys.path.selected)
+            emit('fs:make', state.sys.path.selected)
+          }
+        })
+      ],
+      strings.en.title,
+      emit )
+    const browserFooter = toolbar ()
+    const sidebar = {
+      toolbar: browserToolbar,
+      footer: browserFooter,
+      view: editor(state, emit)
+    }
+
+    const editorToolbar = toolbar( null, null, null, emit)
+
+
+    const editor = {
+      toolbar: editorToolbar,
+      footer: toolbar ()
+    }
+
+    return elements
+  }
 }
