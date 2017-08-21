3f4da2b1cea87900dd54a9580a7ca34c09de2301
Cade <cade@shiba.computer>
2017-08-21 22:36:42 +0200

    making a lot of stuff as generic as possible.

---
+++ components/button/index.js
@@ -0,0 +1,32 @@
+const html = require('choo/html')
+const css = require('sheetify')
+
+module.exports = (props, click) => {
+
+  const base = css`
+    :host {
+      border: none;
+      background: none;
+      color: var(--c);
+      padding: 0;
+    }
+    :host svg {
+      fill: currentColor;
+      height: 1.5rem;
+      width: 1.5rem;
+    }
+    :host:active {
+      color: var(--w);
+    }
+  `
+
+  if (typeof click === 'function') props.click = click
+
+  return html`
+    <button onclick=${props.click} class=${base} name=${props.name}>
+      <svg>
+        <use xlink:href="#txt-${props.icon}" />
+      </svg>
+    </button>
+  `
+}
---
+++ components/view/index.js
@@ -0,0 +1,26 @@
+const html = require('choo/html')
+const style = require('./style')
+
+module.exports = view
+
+// view
+// Elements:
+//    - Toolbar: The toolbar. Is wrapped in the top of the view
+//    - Footer: The footer of a sidebar view.
+//    - View: Pass the view you'd like.
+function view (state, emit, role, elements) {
+
+  return html`
+    <main data-role=${role} class=${style}>
+      <header>
+        ${ elements.toolbar }
+      </header>
+      <div>
+        ${ elements.interface }
+      </div>
+      <footer>
+        ${ elements.footer }
+      </footer>
+    </main>
+  `
+}
