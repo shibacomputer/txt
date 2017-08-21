bff0e36565b79e991fdccd6eedcf652db639e743
Cade <cade@shiba.computer>
2017-08-21 22:37:46 +0200

    Cleanup index.js, getting ready to add packaging, etc

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ const window = require('electron-window')
 
 // Browserify service for development
 const budo = require('budo')
+const Env = require('envobj')
 
 // Get setup items.
-const windowConfig = require('./config/defaults')
+const winCfg = require('./config/defaults')
 const menuConfig = require('./config/menu')
 
 // Settings
@@ -25,43 +26,6 @@ const settings = require('electron-settings')
 global.settings = settings
 
 // Setup windows
-const mainWindowConfig = {
-  titleBarStyle:  windowConfig.win.app.titleBar,
-  width:          windowConfig.win.app.x,
-  height:         windowConfig.win.app.y,
-  minWidth:       windowConfig.win.app.minX,
-  minHeight:      windowConfig.win.app.minY,
-  frame:          windowConfig.win.app.frame
-}
-
-const setupWindowConfig = {
-  titleBarStyle:  windowConfig.win.setup.titleBar,
-  width:          windowConfig.win.setup.x,
-  height:         windowConfig.win.setup.y,
-  minWidth:       windowConfig.win.setup.minX,
-  minHeight:      windowConfig.win.setup.minY,
-  frame:          windowConfig.win.setup.frame,
-  center:         windowConfig.win.setup.centered,
-  resizable:      windowConfig.win.setup.canResize,
-  fullscreenable: windowConfig.win.setup.fullscreen,
-  minimizable:    windowConfig.win.setup.minimizable,
-  maximizable:    windowConfig.win.setup.maximizable
-}
-
-const lockWindowConfig = {
-  titleBarStyle:  windowConfig.win.lock.titleBar,
-  width:          windowConfig.win.lock.x,
-  height:         windowConfig.win.lock.y,
-  minWidth:       windowConfig.win.lock.minX,
-  minHeight:      windowConfig.win.lock.minY,
-  frame:          windowConfig.win.lock.frame,
-  center:         windowConfig.win.lock.centered,
-  movable:        windowConfig.win.lock.moveable,
-  fullscreenable: windowConfig.win.lock.fullscreen,
-  minimizable:    windowConfig.win.lock.minimizable,
-  maximizable:    windowConfig.win.lock.maximizable
-}
-
 const opts = {
   debug: process.env.NODE_ENVIRONMENT !== 'production',
   verbose: true,
@@ -85,30 +49,26 @@ let mainWin
 let lockWin
 let setupWin
 
-
 app.on('ready', () => {
 
   // Check for settings defaults
   if (!settings.has('workingPath')) {
     settings.setAll({
-      'workingPath': path.join(app.getPath('home') + '/Txt'),
       'active': false,
-      'keychain': false,
-      'theme': 'dark'
+      'keychain': false
     })
   }
 
   var active = settings.get('active')
 
   // Get our windows in order, since any of the three can be called at any time.
-  mainWin = window.createWindow(mainWindowConfig)
-  setupWin = window.createWindow(setupWindowConfig)
-  lockWin = window.createWindow(lockWindowConfig)
+  mainWin = window.createWindow(winCfg.main)
+  setupWin = window.createWindow(winCfg.setup)
+  lockWin = window.createWindow(winCfg.lock)
 
   // Setup menus
   const commonMenu = Menu.buildFromTemplate(menuConfig.commonMenu)
   Menu.setApplicationMenu(commonMenu)
-
 
   // Start the dev server
   var server = budo('app.js', opts)
