b9a984f8f96275f533fc4b7113fb09960bf48832
Cade <cade@shiba.computer>
2018-04-30 12:42:57 +0200

    Moved encryption into main process

--- main/pgp.js
+++ main/pgp.js
@@ -11,38 +11,68 @@ openpgp.config.use_native = true
 const APP_NAME = process.env.npm_package_name
 const APP_VERSION = process.env.npm_package_version
 const PGP_COMPRESSION = openpgp.enums.compression.zip
+const PGP_BITS = 4096
 const KEY_FILENAME = '.txtkey'
+
 
 var key, privkey, pubkey, pubKeyObj, privKeyObj, name, email
 
 module.exports = {
-  getKey: function(uri) {
+  getKey: async function(uri) {
     fs.readFile(path.join(uri + '/' + KEY_FILENAME), (err, data) => {
       key = JSON.parse(data.toString('utf8'))
       setupKeysForUse(key)
-      /*
-      console.log(key)
-      var userStr = key.users[0].userId.userid
-      console.log(key.users[0].userId.userid)
-      email = userStr.substring(userStr.lastIndexOf('<') + 1, userStr.lastIndexOf('>'))
-      name = userStr.substring(0, userStr.lastIndexOf(' '))
-      console.log(email, name)
-      */
     })
   },
 
-  generateKey: function ( ) {
+  generateKey: async function (uri, user) {
+    const writeUri = path.join(uri + '/' + KEY_FILENAME)
+    const passphrase = await getSecret()
+
+    name = user.name
+    email = user.email
+
+    const options = {
+      userIds: [{ name:name, email:email }],
+      numBits: PGP_BITS,
+      passphrase: passphrase
+    }
+
+    key = await openpgp.generateKey(options)
+    writeKeytoDisk(key, writeUri)
 
   },
 
-  encrypt: function(contents) {
+  encrypt: async function(contents) {
+    if (!pubKeyObj.primaryKey.decrypted) await decryptKey()
 
+    const options = {
+      data: contents,
+      publicKeys: pubKeyObj.keys,
+      privateKeys: [privKeyObj],
+      compression: PGP_COMPRESSION
+    }
+
+    const ciphertext = await openpgp.encrypt(options)
+    return ciphertext.data
   },
 
-  decrypt: function(encrypted) {
+  decrypt: async function(contents) {
+    if (!pubKeyObj.primaryKey.decrypted) await decryptKey()
 
+    const options = {
+      message: new Uint8Array([contents]),
+      privateKeys: [privKeyObj]
+    }
+
+    const decrypted = await openpgp.decrypt(options)
+    return decrypted.data
+  },
+
+  updateKeychain: async function(secret) {
+    const result = keytar.setPassword(APP_NAME, name, secret)
+    return result
   }
-
 }
 
 function setupKeysForUse (unprocessedKey) {
@@ -59,19 +89,21 @@ function setupKeysForUse (unprocessedKey) {
   decryptKey()
 }
 
-async function getSecret() {
-  keytar.getPassword(APP_NAME, name).then( (secret) => {
-    console.log(secret)
-    return secret
+async function writeKeyToDisk(key, uri) {
+  fs.writeFile(uri, key, (err) => {
+    if (err) console.log(err)
+    else setupKeysForUse(key)
   })
-
 }
 
-function decryptKey () {
-  privKeyObj.decrypt(getSecret()).then ( (result) => {
-    console.log(result)
-  }).catch( (err) => {
-    console.log(err)
-  })
+async function getSecret() {
+  const secret = keytar.getPassword(APP_NAME, name)
+  return secret
+}
 
+async function decryptKey() {
+  const passphrase = await getSecret()
+  const decryptedKey = await privKeyObj.decrypt(passphrase)
+
+  return decryptedKey
 }
