4ee27b30f9285bd092bebcccb3662ef1433b57fe
Cade <cade@shiba.computer>
2018-06-07 00:25:11 +0200

    Added lockscreen
    Signed-off-by: Cade <cade@shiba.computer>

--- renderer/lock/components/lock/style.js
+++
@@ -1,6 +0,0 @@
-const css = require('csjs-inject')
-
-const style = css`
-  
-`
-module.exports = style
---
+++ renderer/lock/state.js
@@ -0,0 +1,8 @@
+module.exports = state
+const { ipcRenderer } = require('electron')
+const pgp = require('../_utils/crypto')
+
+function state (state, emitter) {
+
+
+}
